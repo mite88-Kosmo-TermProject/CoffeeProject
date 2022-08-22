d14664f51e5a83d73e44e1b68ba176d0f7e62e66
hoyounglee <ghdud939@gmail.com>
2022-08-22 21:11:09 +0900

    메인페이지 검색기능까지 추가

--- src/main/java/com/coffice/user/controller/CafeController.java
+++ src/main/java/com/coffice/user/controller/CafeController.java
@@ -34,25 +34,78 @@ public class CafeController {
 	private CafeImpl cafeImpl;
 	StoresDTO dto = new StoresDTO();
 
-//	//지도검색
-//	@RequestMapping(value = "/cafe/map.do", method = RequestMethod.GET)
-//	public String map() {
-//	
-//
-//		return "/user/cafe/map";
-//	}
-//	
-//	//일반검색
-//	@RequestMapping(value = "/cafe/list.do", method = RequestMethod.GET)
-//	public String list() {
-//	
-//
-//		return "/user/cafe/list";
-//	}
+
+	//일반검색
+	@RequestMapping(value = "/cafe/list.do", method = RequestMethod.POST)
+	public String list(Model model, HttpServletRequest req, HttpServletResponse resp) {
+		
+		SearchDTO searchDTO = new SearchDTO();
+
+		//작성한 검색어 파라미터로 받아오기
+		String searchTxt = req.getParameter("searchTxt");
+		// 검색어 파리미터가 없을시 공백문자열로 처리
+		if (searchTxt == "null") {
+			searchTxt = "";
+		}
+		System.out.println("검색어"+searchTxt);
+		// 입점여부 플래그
+		String exAFLTD = req.getParameter("구독여부");
+		
+		System.out.println("처음받은 AFLTD="+ exAFLTD);
+		
+		// 없을시 기본값인 0으로 초기화
+		if (exAFLTD == null || exAFLTD.equals("null")) {
+			exAFLTD = "0";
+		}
+		//문자열 정수변환
+		int AFLTD = Integer.parseInt(exAFLTD);
+		System.out.println("afltd="+AFLTD);
+		
+		//플래그 저장
+		model.addAttribute("AFLTD",AFLTD);
+		//매퍼에 넘길 파라미터 세팅
+		searchDTO.setSearchTxt(searchTxt);
+		//매퍼에 넘길 파라미터 세팅
+		searchDTO.setStore_afltd(AFLTD);
+
+		System.out.println(searchDTO.getSearchTxt());
+
+		//해당되는 게시물의 총 갯수
+		int totalRecordCount = sqlSession.getMapper(CafeSearchImpl.class).getTotalCountSearch(searchDTO);
+		System.out.println(totalRecordCount);
+		//페이지 설정
+		int pageSize = 10;
+		int blockPage = 5;
+		int totalPage = (int) Math.ceil((double) totalRecordCount / pageSize);
+		int nowPage = req.getParameter("nowPage") == null ? 1 : Integer.parseInt(req.getParameter("nowPage"));
+		int start = (nowPage - 1) * pageSize + 1;
+		int end = nowPage * pageSize;
+		searchDTO.setStart(start);
+		searchDTO.setEnd(end);
+		System.out.println(start);
+		System.out.println(end);
+
+		//검색어 기반 가게 정보 출력
+		ArrayList<SearchDTO> lists = sqlSession.getMapper(CafeSearchImpl.class).listPageSearch(searchDTO);
+
+		//페이지 버튼 누를시 이동할 경로 선택
+		String pagingImg = PagingUtil.pagingImg(totalRecordCount, pageSize, blockPage, nowPage,
+				req.getContextPath() + "/cafe/search.do?searchTxt=" + searchDTO.getSearchTxt() + "&&구독여부="+AFLTD+"&&");
+		
+		//추출한 정보 model 객체에 저장
+		model.addAttribute("pagingImg", pagingImg);
+		model.addAttribute("searchTxt", searchDTO.getSearchTxt());
+		model.addAttribute("totalRecordCount", totalRecordCount);
+
+		model.addAttribute("lists", lists);
+		
+
+		return "user/cafe/map";
+	}
 
 	// 처음 리스트출력
 	@RequestMapping("/cafe/map.do")
-	public String list(Model model, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public String maplist(Model model, HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
 		System.out.println("처음 컨트롤러는 실행?");
 		
